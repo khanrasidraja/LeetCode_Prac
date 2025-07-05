a50b11e0e904be864481140a51334c2dd6659ed5
Rasid Raja Khan <khanrasidraja@gmail.com>
2025-07-05 19:46:29 +0530

    Initial commit

---
+++ pact/topKElement.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class topKElement {
+    public static void main(String args[]){
+        int[] nums = {3,2,3,1,2,4,5,5,6};
+        int k = 4;
+        int ans = topKelement(nums,k);
+        System.out.print(ans);
+    }
+    public static int topKelement1(int[] nums, int k){
+        PriorityQueue<Integer> max = new PriorityQueue<>();
+        for(int n :  nums){
+            max.add(n);
+            if(max.size()>k)
+                max.poll();
+        }
+        return max.peek();
+    }
+
+    public static int topKelement(int[] nums, int k){
+        PriorityQueue<Integer> largest = new PriorityQueue<>();
+        for(int num : nums){
+            largest.add(num);
+            if(largest.size()>k){
+                largest.poll();
+            }
+        }
+        return largest.peek();
+    }
+}
